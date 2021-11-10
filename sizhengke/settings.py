31fa52db3cca12bca0a256032328dcbfd27d3d3f
raynor <raynor@prismslight.com>
2021-11-10 17:08:17 +0800

    1.增加 layui 2.静态配置

--- sizhengke/settings.py
+++ sizhengke/settings.py
@@ -134,6 +134,10 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = '/root/sizhengke/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
